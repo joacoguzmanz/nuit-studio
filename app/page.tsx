99a49698a6394d249b8f509f91da5aa7ddecc006
Joaquin Guzman <joaquingusman77@gmail.com>
2024-04-29 23:29:29 +0200

    first commit - done desktop coming soon page

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,42 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const Home = () => {
+    return (
+        <main className='container'>
+            <div className='container-left'>
+                <div className="content-top">
+                    <Link href='/'>Nuit Studio</Link>
+                </div>
+                <div className="content-body">
+                    <div className="description">
+                        <h1>En construcción...</h1>
+                        <p>Estamos preparando nuestro sitio, pero podés contactarnos para convesar sobre tus
+                            necesidades y comenzar a dar forma a tu proyecto.</p>
+                    </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                    <div className="contact-container">
+                        <div className="mail-contact">
+                            <h2 className="contact-title">Dejanos un correo</h2>
+                            <a href="mailto:contact@gmail.com" className="contact-text">hola@nuitstudio.com</a>
+                        </div>
+                        <div className="phone-contact">
+                            <h2 className="contact-title">Envianos un mensaje</h2>
+                            <a href="#" className="contact-text">+34 692 95 55 04</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="content-bottom">
+                    <p className="location-footer">Barcelona, ESP</p>
+                    <p className="location-footer">Buenos Aires, ARG</p>
+                </div>
+            </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            <div className='container-right'>
+                <Image src={'/soon-image.jpg'} alt={'Galaxy image'} fill={true} objectFit={'cover'}/>
+            </div>
+        </main>
+    );
 }
+
+export default Home;
